6e783090e8eb4c66146e0370f628373374265f3e
MalliShiva <44941152+MalliShiva@users.noreply.github.com>
2018-12-15 08:44:13 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,11 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-  
-	/**
-	 *@param someone the name of a person
-	 *@return someone string
-	**/
+
+/**
+*@param someone the name of a person
+*@return someone string
+**/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
